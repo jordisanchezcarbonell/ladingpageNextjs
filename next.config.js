30d29aabcaf4bbaf141692f5d4a4456f83f95e25
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2022-11-20 16:49:18 +0100

    First commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "next-themes";
+import "../css/tailwind.css";
+
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
