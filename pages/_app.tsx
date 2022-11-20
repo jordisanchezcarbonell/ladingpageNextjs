e67903253308e8a94c327736de27c02677fc252e
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2022-11-20 17:13:07 +0100

    Add `@vercel/analytics` to your project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
