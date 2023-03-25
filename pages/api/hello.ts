5b9a83155bfb76d6418e6c41d6a40a0d6b1e406b
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2023-03-25 13:26:37 +0100

    Prueba logtails.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+const { Node: Logtail } = require("@logtail/js");
+const logtail = new Logtail("FyvbCfevrmsNehWqLUMdmUpU");
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  logtail.info(
+    "Log message with structured data. dese la api231313132131231231231 ",
+    {
+      item: "Orange Soda",
+      price: 100.0,
+    }
+  );
+  res.status(200).json({ name: "John Doe" });
 }
